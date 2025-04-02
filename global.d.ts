69ba66130a8459e7ad1c308c4d5aa2f8ee45abb9
bspasic74 <b.spasic74@gmail.com>
2025-04-02 14:11:32 +0200

    drizzle db update

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv extends CloudflareEnv {
+        }
+    }
+}//extends process.env to have new types given by cloudflare
+
+export type {};
