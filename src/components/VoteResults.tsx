cfbf490b21d9a667ada8f27b99457c9fb59c1e54
bspasic74 <b.spasic74@gmail.com>
2025-04-30 14:50:42 +0200

    Form nad votes update

--- src/components/VoteResults.tsx
+++ src/components/VoteResults.tsx
@@ -12,8 +12,8 @@ export default function VoteResults({ yesVotes, totalVotes }: VoteResultsProps) 
     <div className="mt-6 p-4 bg-gray-100 rounded-md">
       <h2 className="text-xl font-bold mb-2">Voting Results</h2>
       <div className="flex flex-row text-lg">
-        <div className='pr-5 border-r'>✅ Yes votes: <span className="font-semibold">{yesVotes}</span></div>
-        <div className='pr-5 pl-5 border-r'>❌ No votes: <span className="font-semibold">{noVotes}</span></div>
+        <div className='pr-5 border-r'>✅ Yes votes: <span className="font-semibold">{noVotes}</span></div>
+        <div className='pr-5 pl-5 border-r'>❌ No votes: <span className="font-semibold">{yesVotes}</span></div>
         <div className='pr-5 pl-5'>🗳️ Total votes: <span className="font-semibold">{totalVotes}</span></div>
       </div>
     </div>
